4b933608bbc42d26d6691a59897776da698f03fe
gdpark14 <kyudong0104@gmail.com>
2022-01-05 11:43:38 +0900

    latency estimation

--- DigitClassifier/app/src/main/java/com/example/digitclassifier/MainActivity.java
+++ DigitClassifier/app/src/main/java/com/example/digitclassifier/MainActivity.java
@@ -4,49 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import com.example.digitclassifier.draw.DrawActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = (EditText)findViewById(R.id.input_Et);
-        Button button = (Button)findViewById(R.id.input_Btn);
-        TextView textView = (TextView)findViewById(R.id.output_Tv);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String text = editText.getText().toString();
-                textView.setText(text);
-
-//                try {
-//                    Thread.sleep(10 * 1000);
-//                    Toast.makeText(MainActivity.this, "complete", Toast.LENGTH_SHORT).show();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-
-            }
+        Button drawBtn = findViewById(R.id.drawBtn);
+        drawBtn.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, DrawActivity.class);
+            startActivity(i);
         });
-
-//        button.setOnClickListener(view -> {
-//        });
-
-        Button actBtn = findViewById(R.id.activity_launch_Btn);
-        actBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, MyActivity.class);
-                startActivity(i);
-            }
-        });
-
     }
 }
